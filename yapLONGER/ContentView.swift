fbdb93093c45029c08228a5cd3ca90713ce68012
adamzafir <adam_zafir_mohammed_sudhir+github@s2024.ssts.edu.sg>
2025-11-10 13:07:39 +0800

    Initial Commit

---
+++ yapLONGER/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yapLONGER
+//
+//  Created by Adam Zafir on 11/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yapLONGER/yapLONGERApp.swift
@@ -0,0 +1,17 @@
+//
+//  yapLONGERApp.swift
+//  yapLONGER
+//
+//  Created by Adam Zafir on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct yapLONGERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
