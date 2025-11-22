94c7e5a89a699dda7f1b4c4b1f1a66efe4a010e6
adamzafir <adam_zafir_mohammed_sudhir+github@s2024.ssts.edu.sg>
2025-11-22 11:52:06 +0800

    watchos and viewmodel

---
+++ Scripties Watchos Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scripties Watchos Watch App
+//
+//  Created by Adam Zafir on 11/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scripties Watchos Watch App/Scripties_WatchosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scripties_WatchosApp.swift
+//  Scripties Watchos Watch App
+//
+//  Created by Adam Zafir on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Scripties_Watchos_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
