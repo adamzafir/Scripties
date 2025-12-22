5ced2a45aba169a2f038ab46f6c7b6e902f6affe
adamzafir <adam_zafir_mohammed_sudhir+github@s2024.ssts.edu.sg>
2025-12-22 18:23:07 +0800

    Add website files

--- Scripties Watchos Watch App Extension/ExtensionDelegate.swift
+++
@@ -1,7 +0,0 @@
-import WatchKit
-
-class ExtensionDelegate: NSObject, WKExtensionDelegate {
-    func applicationDidFinishLaunching() {}
-    func applicationDidBecomeActive() {}
-    func applicationWillResignActive() {}
-}
